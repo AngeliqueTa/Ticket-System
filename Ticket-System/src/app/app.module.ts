38eb488ede8706fbce8b4c79c7a15937bf65d86a
ANGELIQUE-LAPTO\Angelique <Blushy777@gmail.com>
2022-05-18 12:10:37 +0200

    App: component, modules

--- Ticket-System/src/app/app.module.ts
+++ Ticket-System/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './component/users/users.component';
+import { TicketsComponent } from './component/tickets/tickets.component';
+import { NavigationComponent } from './component/navigation/navigation.component';
+import { ProjectsComponent } from './component/projects/projects.component';
+import { ReportsComponent } from './component/reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    TicketsComponent,
+    NavigationComponent,
+    ProjectsComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,
